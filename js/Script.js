39d6a3df5788887189dc3c056d2fcf0378bd2c6b
omarak <omerakrum02@gmail.com>
2023-10-24 15:31:28 +0300

    update js file

--- js/Script.js
+++ js/Script.js
@@ -1,7 +1,20 @@
 
 $(document).ready(function (){
+
     $('header nav ul li a').on('click',function (){
-        $('header nav ul li a').removeClass('active');
-        $(this).addClass('active');
+        $(this).addClass('active').parent().siblings().find('a').removeClass('active');
+    });
+
+
+    $(window).on('scroll',function (){
+        var win = $(window).scrollTop();
+        console.log(win);
+
+        if (win <= 300) {
+            $("header nav ul li a:contains('Home')").addClass('active');
+        }
+        else {
+            $("header nav ul li a:contains('Home')").removeClass('active');
+        }
     });
 });
